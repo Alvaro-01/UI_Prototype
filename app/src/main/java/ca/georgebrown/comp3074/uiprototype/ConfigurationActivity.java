0b34e8e0f0032c616bb531acb174d9e86258d824
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2023-12-07 22:05:16 -0500

    Ui prototype ver 2.4 in sql works, just need make the output appear more neatly

--- app/src/main/java/ca/georgebrown/comp3074/uiprototype/ConfigurationActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/uiprototype/ConfigurationActivity.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
-    private ListView listView;
+
 
 
     @Override
@@ -29,23 +29,47 @@ public class ConfigurationActivity extends AppCompatActivity {
 
         add = findViewById(R.id.add);
         delete = findViewById(R.id.delete);
-
+        lv_menulist = findViewById(R.id.lv_menulist);
         et_category = findViewById(R.id.et_category);
         et_name = findViewById(R.id.et_name);
         et_number = findViewById(R.id.et_number);
+
+        DBHelper dbHelper = new DBHelper(ConfigurationActivity.this);
+
+        // Retrieve items from the database
+        List<MenuModel> items = dbHelper.getAllItems();
+
+        // Create an ArrayAdapter and set it to the ListView
+        ArrayAdapter<MenuModel> menuArrayAdapter = new ArrayAdapter<>(ConfigurationActivity.this, android.R.layout.simple_list_item_1, items);
+        lv_menulist.setAdapter(menuArrayAdapter);
+
+
+        //clicking add button
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                MenuModel menuModel;
                 try {
                     String category = et_category.getText().toString();
                     String name = et_name.getText().toString();
                     int number = Integer.parseInt(et_number.getText().toString());
 
-                    MenuModel menuModel = new MenuModel(category, name, number);
+                    menuModel = new MenuModel(category, name, number);
+
+                    DBHelper dbHelper = new DBHelper(ConfigurationActivity.this);
+                    boolean success = dbHelper.addOne(menuModel);
+                    Toast.makeText(ConfigurationActivity.this,"Success ="+success,Toast.LENGTH_SHORT).show();
+
                 }catch (Exception e){
                     Toast.makeText(ConfigurationActivity.this,"error creating item",Toast.LENGTH_SHORT).show();
+
                 }
+
+
+
+
+
             }
         });
 
--- app/src/main/java/ca/georgebrown/comp3074/uiprototype/DBHelper.java
+++ app/src/main/java/ca/georgebrown/comp3074/uiprototype/DBHelper.java
@@ -1,24 +1,29 @@
 package ca.georgebrown.comp3074.uiprototype;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
     // Database version
     private static final int DATABASE_VERSION = 1;
 
     // Database name
-    private static final String DATABASE_NAME = "YourDatabaseName";
+    private static final String DATABASE_NAME = "Menu.db";
 
     // Table name
-    private static final String TABLE_NAME = "YourTableName";
+    private static final String TABLE_NAME = "MENU_TABLE";
 
     // Columns
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_CATEGORY = "category";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_NUMBER = "number";
+    private static final String COLUMN_ID = "MENU_ID";
+    private static final String COLUMN_CATEGORY = "MENU_CATEGORY";
+    private static final String COLUMN_NAME = "MENU_NAME";
+    private static final String COLUMN_PRICE = "MENU_PRICE";
 
     // Constructor
     public DBHelper(Context context) {
@@ -32,7 +37,7 @@ public class DBHelper extends SQLiteOpenHelper {
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + COLUMN_CATEGORY + " TEXT,"
                 + COLUMN_NAME + " TEXT,"
-                + COLUMN_NUMBER + " TEXT"
+                + COLUMN_PRICE + " TEXT"
                 + ")";
         db.execSQL(CREATE_TABLE_QUERY);
     }
@@ -43,5 +48,52 @@ public class DBHelper extends SQLiteOpenHelper {
         // Drop older table if exists and recreate the table
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean addOne(MenuModel menuModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_CATEGORY, menuModel.getCategory());
+        cv.put(COLUMN_NAME, menuModel.getName());
+        cv.put(COLUMN_PRICE, menuModel.getPrice());
+
+        long insert = db.insert(TABLE_NAME,null,cv);
+        if(insert == -1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public List<MenuModel> getAllItems() {
+        List<MenuModel> returnList = new ArrayList<>();
+
+        String CREATE_TABLE_QUERY = "SELECT * FROM "+ TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+         Cursor cursor = db.rawQuery(CREATE_TABLE_QUERY,null);
+
+         if(cursor.moveToFirst()){
+             do{
+
+                 String menuCategory = cursor.getString(1);
+                 String menuName = cursor.getString(2);
+                 int menuPrice = cursor.getInt(3);
+
+                 MenuModel newItem = new MenuModel(menuCategory,menuName,menuPrice);
+                 returnList.add(newItem);
+             }while (cursor.moveToNext());
+
+
+         }else{
+             //nothing happens
+         }
+
+         //close cursor and db
+        cursor.close();
+        db.close();
+        return returnList;
     }
 }
